ca5110e9622787da3db114c946b4f2fa0c5bdfa7
EmadIdris <emadidris@outlook.com>
2021-08-03 13:59:44 +0300

    addyn

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 let Name = prompt("Enter Your Name : ");
 alert("Welcome "+Name);
-alert("Questions about Emad \n Answer Yes Or No");
+alert("Questions about Emad \n Answer Yes/y  Or No/n  ");
 document.write("Welcome "+Name);
 document.write('<hr>')
 console.log("Welcome "+Name);
@@ -10,14 +10,15 @@ console.log("Welcome "+Name);
 let  eat = prompt("Emad Like To Eat Mansaf?").toLowerCase();
 switch (eat) {
     case 'yes':
+        case 'y':
         alert("Correct Answer");
         document.write("Emad Like To Eat Mansaf : Yes ")
         console.log("Correct Answer");
         break;
     case 'no':
+        case 'n':
         alert("Incorrect Answer");
         document.write("Incorrect Answer")
-        
         break;
     default:
         alert("None")
@@ -31,11 +32,13 @@ document.write('<br>')
 let wakeUp = prompt("Emad Like To Get Up Early?").toLowerCase();
 switch (wakeUp) {
     case 'yes':
+        case 'y':
         alert("Incorrect Answer");
         document.write("Incorrect Answer")
         
         break;
     case 'no':
+        case 'n':
             alert("Correct Answer");
             document.write("Emad Like To Get Up Early : No")
             console.log("Correct Answer");
@@ -52,11 +55,13 @@ document.write('<br>')
 let Song= prompt("Emad Like To Listen To Songs?").toLowerCase();
 switch (Song) {
     case 'yes':
+        case 'y':
         alert("Correct Answer");
         document.write("Emad Like To Listen To Songs : Yes ")
         console.log("Correct Answer");
         break;
     case 'no':
+        case 'n':
             alert("Incorrect Answer");
             document.write("Incorrect Answer")
             
@@ -73,11 +78,13 @@ document.write('<br>')
 let Sports= prompt("Emad Like To Play Sports?").toLowerCase();
 switch (Sports) {
     case 'yes':
+        case 'y':
         alert("Correct Answer");
         document.write("Emad Like To Play Sports : Yes ")
         console.log("Correct Answer");
         break;
     case 'no':
+        case 'n':
             alert("Incorrect Answer");
             document.write("Incorrect Answer")
             
@@ -94,11 +101,13 @@ document.write('<br>')
 let Movies= prompt("Emad Like To Watch Movies?").toLowerCase();
 switch (Movies) {
     case 'yes':
+        case 'y':
         alert("Correct Answer");
         document.write("Emad Like To Watch Movies : Yes ")
         console.log("Correct Answer");
         break;
     case 'no':
+        case 'n':
             alert("Incorrect Answer");
             document.write("Incorrect Answer")
             
@@ -109,3 +118,6 @@ switch (Movies) {
         
         break;
 }
+
+let chooseNumber = 
+
